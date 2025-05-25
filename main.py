d0c46a5bd6455f7b6bd5064cc4646e03b55f1d57
JijaEdem666 <egorvmamontov@gmail.com>
2025-05-25 18:29:42 +0400

    app variable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from service.ServiceHost import ServiceHost
 
+host = ServiceHost()
+app = host.app
+
 if __name__ == "__main__":
-    host = ServiceHost()
     host.start()
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Делает сервер доступным на любом интерфейсе, порт берётся из переменных Render
-uvicorn main:host.app --host 0.0.0.0 --port ${PORT:-10000}
+uvicorn main:app --host 0.0.0.0 --port ${PORT:-10000}
